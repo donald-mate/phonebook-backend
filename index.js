2d6674b751050037e0bb819688cbd98ef4bb4c86
Donald Mate <donald.mate@gmail.com>
2021-04-02 17:45:00 -0400

    added put functionality on backend

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
-app.use(express.static("build"));
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -16,7 +16,7 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -25,10 +25,7 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -71,14 +68,43 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put("/api/persons/:id"),
+  (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then((updatedPerson) => {
+        response.json(updatedPerson);
+        console.log(updatedPerson);
+      })
+      .catch((error) => next(error));
+  };
+
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
